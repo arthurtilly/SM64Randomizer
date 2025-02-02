71562cce10a37553d080699171c56c6e9a270813
arthurtilly <arthurtilly413@gmail.com>
2025-02-02 17:27:14 +1300

    Exploding shell

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -1459,18 +1459,21 @@ u32 interact_koopa_shell(struct MarioState *m, UNUSED u32 interactType, struct O
 
         if (interaction == INT_HIT_FROM_ABOVE || m->action == ACT_WALKING
             || m->action == ACT_HOLD_WALKING) {
-            m->interactObj = obj;
-            m->usedObj = obj;
-            m->riddenObj = obj;
+            // m->interactObj = obj;
+            // m->usedObj = obj;
+            // m->riddenObj = obj;
 
-            attack_object(obj, interaction);
-            update_mario_sound_and_camera(m);
-            play_shell_music();
-            mario_drop_held_object(m);
+            // attack_object(obj, interaction);
+            // update_mario_sound_and_camera(m);
+            // play_shell_music();
+            // mario_drop_held_object(m);
 
-            //! Puts Mario in ground action even when in air, making it easy to
-            // escape air actions into crouch slide (shell cancel)
-            return set_mario_action(m, ACT_RIDING_SHELL_GROUND, 0);
+            // //! Puts Mario in ground action even when in air, making it easy to
+            // // escape air actions into crouch slide (shell cancel)
+            // return set_mario_action(m, ACT_RIDING_SHELL_GROUND, 0);
+            struct Object *explosion = spawn_object(obj, MODEL_EXPLOSION, bhvExplosion);
+            explosion->oGraphYOffset += 100.0f;
+            obj->activeFlags = ACTIVE_FLAG_DEACTIVATED;
         }
 
         push_mario_out_of_object(m, obj, 2.0f);
