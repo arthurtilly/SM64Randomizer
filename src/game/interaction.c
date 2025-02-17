28475590ad2535be45365aa0caac9faedc2acd30
Zoey Zolotova <zoey.zolotova@gmail.com>
2025-02-17 21:31:06 +1100

    Remove unused level lock dialog code.

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -840,7 +840,7 @@ u32 interact_star_or_key(struct MarioState *m, UNUSED u32 interactType, struct O
             fadeout_level_music(126);
         
             if (!isNew) {
-                set_course_is_locked(gCurrCourseNum, DIALOG_143);
+                set_course_is_locked(gCurrCourseNum);
             }
         }
 
