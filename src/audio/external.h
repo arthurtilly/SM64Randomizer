b8c8f4785d8669c3f8ca54900eadecb32483922a
arthurtilly <arthurtilly413@gmail.com>
2025-02-18 10:35:25 +1300

    small fix

--- src/audio/external.h
+++ src/audio/external.h
@@ -14,7 +14,7 @@
 
 #define SEQUENCE_ARGS_R(priority, seqId) ((priority << 8) | \
     ((gOptionsSettings.cosmetic.s.musicOn == 1) ? \
-        (((seqId) & 0x80) | gRandomSongs[random_u16_seeded(gRandomizerGameSeed - (seqId)) % sizeof(gRandomSongs)]) \
+        gRandomSongs[random_u16_seeded(gRandomizerGameSeed - (seqId)) % sizeof(gRandomSongs)] \
     : (gOptionsSettings.cosmetic.s.musicOn == 2 ? \
         0 \
     : seqId)))
