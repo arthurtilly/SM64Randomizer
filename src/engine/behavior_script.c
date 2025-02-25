d75a3fd515128b401aea82a2a1c08daee8519264
Zoey Zolotova <zoey.zolotova@gmail.com>
2025-02-25 16:33:32 +1100

    Fix cannon opening bobomb buddies having red eyes sometimes.

--- src/engine/behavior_script.c
+++ src/engine/behavior_script.c
@@ -362,7 +362,8 @@ static s32 bhv_cmd_randomize_object(void) {
     tinymt32_init(&randomState, gRandomizerGameSeed + (gCurrentObject->pointerSeed/4));
 
     f32 r = tinymt32_generate_float(&randomState);
-    s32 isEnemy = gCurrentObject->behavior != segmented_to_virtual(bhvBobombBuddy);
+    s32 isEnemy = gCurrentObject->behavior != segmented_to_virtual(bhvBobombBuddy)
+        && gCurrentObject->behavior != segmented_to_virtual(bhvBobombBuddyOpensCannon);
     // 30% chance to be fast
     o->isFast = 0;
     if (HARDCORE_IRONMARIO && isEnemy) {
