4f97fb99c127db66dc78b927b36278368d6c2775
Zoey Zolotova <zoey.zolotova@gmail.com>
2025-02-25 17:45:37 +1100

    Make fast fire spitters red.

--- src/engine/behavior_script.c
+++ src/engine/behavior_script.c
@@ -363,7 +363,8 @@ static s32 bhv_cmd_randomize_object(void) {
 
     f32 r = tinymt32_generate_float(&randomState);
     s32 isEnemy = gCurrentObject->behavior != segmented_to_virtual(bhvBobombBuddy)
-        && gCurrentObject->behavior != segmented_to_virtual(bhvBobombBuddyOpensCannon);
+        && gCurrentObject->behavior != segmented_to_virtual(bhvBobombBuddyOpensCannon)
+        && gCurrentObject->behavior != segmented_to_virtual(bhvPitBowlingBall);
     // 30% chance to be fast
     o->isFast = 0;
     if (HARDCORE_IRONMARIO && isEnemy) {
